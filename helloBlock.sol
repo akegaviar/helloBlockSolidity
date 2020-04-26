190f983fd2a3f0167e0d89207cfff7bcba46a4d8
Ake Gaviar <evkonst@gmail.com>
2020-04-26 18:43:30 +0300

    Initial commit

---
+++ helloBlock.sol
@@ -0,0 +1,27 @@
+
+pragma solidity ^0.6.1; 
+contract helloBlock 
+{
+    string message; 
+    
+    constructor() 
+    public
+    {
+        message = "Hello Block!";
+    }
+    
+    function printMessage() 
+    public 
+    view 
+    returns (string memory) 
+    {
+        return message;
+    }
+    
+    function setMessage(string memory _message)
+    public 
+    
+    {
+        message = _message;
+    }
+}
